404a39b89304efc396f1216e03b371304dc5a6da
Hugo <hugo-ensc@outlook.com>
2016-11-07 17:35:49 -0600

    api empresa

--- miprimerempleo/urls.py
+++ miprimerempleo/urls.py
@@ -13,9 +13,17 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
+from api import urls as urlsApi
+from django.views.static import serve
+from django.conf import settings
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^api/', include(urlsApi)),
+    url(
+        regex=r'^media/(?P<path>.*)$',
+        view=serve,
+        kwargs={'document_root':settings.MEDIA_ROOT}),
 ]
