b56a56ed5c06d3311b823762ead231f8d42c8b8d
Hugo <hugo-ensc@outlook.com>
2016-11-07 16:29:42 -0600

    modelo empresa terminado

--- empresa/admin.py
+++ empresa/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from .models import BusinessModel
 # Register your models here.
+
+
+class BusinessModelAdmin(admin.ModelAdmin):
+	list_display = ('name', 'tel', 'email', 'name_project_manager',)
+	search_fields = ('name', 'email',)
+	ordering = ['name']
+
+admin.site.register(BusinessModel, BusinessModelAdmin)
