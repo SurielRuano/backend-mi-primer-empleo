c4850147fa7f8faea3a7bc9d054f840eab52d652
Hugo <hugo-ensc@outlook.com>
2016-11-09 23:40:54 -0600

    Se agrego sedeco

--- usuarios/models.py
+++ usuarios/models.py
@@ -1,5 +1,16 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
 class Sedeco(models.Model):
-	pass
+	user = models.OneToOneField(settings.AUTH_USER_MODEL)
+	acceso = models.IntegerField()
+	rol = models.CharField(max_length=80)
+	activate = models.BooleanField(default=True)
+	estado = models.CharField(max_length=80)
+	ip = models.CharField(max_length=15)
+
+	def __str__(self):
+		return self.user
+
+
